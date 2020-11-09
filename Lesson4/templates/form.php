1ad48f91dfb45388951d1c126b2c929ba3214fba
dymoc <dymoc@mail.ru>
2020-11-09 12:01:57 +0300

    путь $post_file_path

--- Lesson4/templates/form.php
+++ Lesson4/templates/form.php
@@ -1,4 +1,4 @@
-<form enctype="multipart/form-data" action="<?= ACTIONS_PATH . 'post_file.php' ?>" method="POST">
+<form enctype="multipart/form-data" action="<?= $post_file_path ?>" method="POST">
      <h3><?= $post_file_path ?></h3>
      <input type="hidden" name="MAX_FILE_SIZE" value="300000" />
      <input name="userfile" type="file" />
